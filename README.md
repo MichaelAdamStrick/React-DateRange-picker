184d6672825de19c2d385b8c5e9a7305d199f57c
michael strickland <60932642+MichaelAdamStrick@users.noreply.github.com>
2020-06-02 09:28:34 -0600

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-DateRange-picker
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import DatePicker from "./date-picker";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <DatePicker />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
